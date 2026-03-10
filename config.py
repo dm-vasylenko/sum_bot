fd6c3323e9c3756cd4133ab41eb448be2c9398a7
Dmytro Vasylenko <d.vasilenko08@gmail.com>
2026-03-10 08:35:40 +0100

    use env vars

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN = "7956662752:AAFcvE8Nr4o7wH4bK380g7q04hi0SRzYz84"     # токен от BotFather
-SOURCE_CHAT_ID = -1001624700032    # ID группы
-TOPIC_ID = 882                      # ID топика
+import os
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+SOURCE_CHAT_ID = int(os.environ["SOURCE_CHAT_ID"])
+TOPIC_ID = int(os.environ["TOPIC_ID"])
